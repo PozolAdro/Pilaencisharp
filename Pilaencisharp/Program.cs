797bb2670b61c762aabb6e57c63a28c6f8b4194b
Adrian <Adrian@DESKTOP-COFL36B>
2023-10-18 09:08:56 -0600

    Agregar archivos de proyecto.

---
+++ Pilaencisharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class program
+{
+    static void Main()
+    { const int capacidad = 10;
+        int[] elementos = new int[capacidad];
+        int tope = -1;
+
+        Console.WriteLine("Teclea elemento de la pila (termina con -1)");
+        int x = 0;
+        const int CLAVE = -1;
+        while (x !=CLAVE)
+        {
+            try
+            {
+
+                x = int.Parse(Console.ReadLine());
+                if (tope<capacidad-1)
+                {
+                    tope++;
+                    elementos[tope] = x;
+                }
+                else
+                {
+                    Console.WriteLine("Excepcion: Pila Llena");
+                    break;
+                }
+            }
+            catch (FormatException)
+            { Console.WriteLine("Excepxion: Entrada no valida");
+            }
+        }
+        if (tope>=0)
+        {
+            Console.Write("Elementos de la pila: ");
+            while (tope>=0)
+            {
+                x = elementos[tope];
+                tope--;
+                Console.Write(x + " ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Pila vacia");
+        }
+        Console.ReadLine();
+    }
+}
